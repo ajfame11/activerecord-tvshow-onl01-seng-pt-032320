718bc4ea85255033a4b762870236eb34df108614
aurel fontaine <ajfame11@gmail.com>
2020-06-10 19:48:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,10 @@
 class Show <ActiveRecord::Base
   
-  def Show.highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
   
-  def most_popular_show
+  def Show.most_popular_show
     
   end
   
